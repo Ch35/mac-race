3aa620859e9ecc6db01543b919a4b49040715cfb
Ryan <ryangray1111@gmail.com>
2026-03-27 13:11:31 +0200

    Timezone fix

--- src/lib/race.ts
+++ src/lib/race.ts
@@ -2,5 +2,9 @@ import { Race } from "@prisma/client";
 
 export const isActive = (race: Race) => {
   const currentTime = Date.now();
-  return new Date(race.start).getTime() <= currentTime && new Date(race.end).getTime() >= currentTime;
+  const started = +new Date(race.start).getTime() <= +currentTime;
+  const notEnded = +new Date(race.end).getTime() >= +currentTime;
+
+  const isActive = started && notEnded;
+  return isActive;
 }
