99f137dff0ddab1f0ab039d339af3ecfa9e91e5a
Ryan <ryangray1111@gmail.com>
2026-03-27 14:09:33 +0200

    Deploy fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 "use client";
 
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import Table from "@/components/Table/Table";
 
-export default function Home() {
+function HomeContent() {
   const searchParams = useSearchParams();
   const autoScroll = searchParams.get("autoscroll") === "true";
 
   return <Table autoScroll={autoScroll} />;
+}
+
+export default function Home() {
+  return (
+    <Suspense fallback={<Table autoScroll={false} />}>
+      <HomeContent />
+    </Suspense>
+  );
 }
